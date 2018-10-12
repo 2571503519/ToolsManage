d4cba10b7543f4d4f8e22f9578330506b8c308ff
Jackaroo_zZ <2571503519@qq.com>
2018-10-12 22:50:56 +0800

    Resource的CURD完成

--- src/main/java/com/jack/pojo/entity/Resource.java
+++ src/main/java/com/jack/pojo/entity/Resource.java
@@ -2,12 +2,14 @@ package com.jack.pojo.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.sql.Timestamp;
 
 /**
  * Created by Jackaroo Zhang on 2018/10/11.
  */
+@ToString
 public class Resource {
 
     @Getter @Setter private Long resId;
--- src/test/java/com/jack/dao/ResourceMapperTest.java
+++ src/test/java/com/jack/dao/ResourceMapperTest.java
@@ -33,5 +33,34 @@ public class ResourceMapperTest {
         logger.info("Test: Save a Resource, Result: {}, resId={}", res, resource.getResId());
     }
 
+    @Test
+    public void testUpdateResource() {
+        Random random = new Random(System.currentTimeMillis());
+        Resource resource = mapper.findResourceByPrimaryKey(3L);
+        if (resource == null) {
+            logger.info("Test: Query Result is null");
+            return;
+        }
+        resource.setResUrl("/api/v" + random.nextInt(10) +"/repertory");
+        boolean res = mapper.updateResource(resource);
+        logger.info("Test: Update a Resource, Result: {}", res);
+    }
+
+    @Test
+    public void testFindResourceByPrimaryKey() {
+        Resource resource = mapper.findResourceByPrimaryKey(3L);
+        logger.info("Test: Query a Resource, Result: {}", resource);
+    }
+
+    @Test
+    public void testDeleteResource() {
+        Resource resource = mapper.findResourceByPrimaryKey(3L);
+        if (resource == null) {
+            logger.info("Test: Query Result is null");
+            return;
+        }
+        boolean res = mapper.deleteResource(resource.getResId());
+        logger.info("Test: Delete a Resource, Result: {}", res);
+    }
 
 }
